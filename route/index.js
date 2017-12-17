cdbac34e521f6b187d025b54d94a99ff1fbdb209
Glitch (glossy-tent) <none>
2017-12-17 09:05:44 +0000

    📷👽 Updated with Glitch

--- route/index.js
+++ route/index.js
@@ -14,6 +14,7 @@ exports.list = function(req, res) {
 };
 // JSON API for getting a single poll
 exports.poll = function(req, res) {
+  console.log(req.params)
   var pollId = req.params.id;
   Poll.findById(pollId, '', { lean: true }, function(err, poll) {
     if(poll) {
@@ -53,5 +54,45 @@ exports.create = function(req, res) {
     } else {
       res.json(doc);
     }
+  });
+};
+///////////////////////////////////
+exports.vote = function(socket) {
+  socket.on('send:vote', function(data) {
+    var ip = socket.handshake.headers['x-forwarded-for'] ||
+      socket.handshake.address.address;
+    Poll.findById(data.poll_id, function(err, poll) {
+      var choice = poll.choices.id(data.choice);
+      choice.votes.push({
+        ip: ip
+      });
+      poll.save(function(err, doc) {
+        var theDoc = {
+          question: doc.question,
+          _id: doc._id,
+          choices: doc.choices,
+          userVoted: false,
+          totalVotes: 0
+        };
+        for (var i = 0, ln = doc.choices.length; i < ln; i++) {
+          var choice = doc.choices[i];
+          for (var j = 0, jLn = choice.votes.length; j < jLn; j++) {
+            var vote = choice.votes[j];
+            theDoc.totalVotes++;
+            theDoc.ip = ip;
+            if (vote.ip === ip) {
+              theDoc.userVoted = true;
+              theDoc.userChoice = {
+                _id: choice._id,
+                text: choice.text
+              };
+            }
+          }
+        }
+        console.log(doc)
+        socket.emit('myvote', theDoc);
+        socket.broadcast.emit('vote', theDoc);
+      }); 
+    });
   });
 };
